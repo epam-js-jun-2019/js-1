dd6f167b8f010b55b426d2d1522e054739d4706b
Azaliya Kasimova <sea-blue@inbox.ru>
2019-07-14 23:25:00 +0300

    function 3 added

--- Azaliya Kasimova/tenfunctions.js
+++ Azaliya Kasimova/tenfunctions.js
@@ -1,20 +1,38 @@
 
-// FIRST FUNCTION
+//FUNCTION #1
 function splitAndMerge(str, sp) {
-  return str.split(" ").map(function(b) { return b.split("").join(sp)}).join(" ");
+  return str.split(" ").map(function(word) { return word.split("").join(sp)}).join(" ");
  }
 
  console.log(splitAndMerge("My name is John", " "));
  console.log(splitAndMerge("Hello World!", ","));
 
- //SECOND FUNCTION
+ //FUNCTION #2
+ 
 
- //FIRD FUNCTION
 
- //FOURTH FUNCTION
+ //FUNCTION #3
+ function toCamelCase(str) {
+   var a = str.search("-");
+    if ( a > 0) {
+      sp = "-";
+    }
+    else {
+       sp = "_";
+    }
+       
+    return str[0] + str.split(sp).map(function(word) { 
+      return word[0].toUpperCase() + word.substr(1);
+    }).join("").substr(1);
+ }
+
+ console.log(toCamelCase("the-stealth-warrior"));
+ console.log(toCamelCase("The_Stealth_Warrior"));
+
+ //FUNCTION #4
  function reverseWords(str) {
-   return str.split(" ").map(function(a) { 
-     return a.split("")}).map(function(c) {return c.reverse().join("")}).join(" ");
+   return str.split(" ").map(function(word) { 
+     return word.split("")}).map(function(char) {return char.reverse().join("")}).join(" ");
  }
 
  console.log(reverseWords(" A fun little challenge! "));
