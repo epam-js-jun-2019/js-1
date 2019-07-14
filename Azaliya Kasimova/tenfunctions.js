577a3e3f5b14c9d20619bd13e90a39867be9c960
Azaliya Kasimova <sea-blue@inbox.ru>
2019-07-14 20:32:14 +0300

    fourth function written

--- Azaliya Kasimova/tenfunctions.js
+++ Azaliya Kasimova/tenfunctions.js
@@ -7,6 +7,18 @@ function splitAndMerge(str, sp) {
  console.log(splitAndMerge("My name is John", " "));
  console.log(splitAndMerge("Hello World!", ","));
 
+ //SECOND FUNCTION
+
+ //FIRD FUNCTION
+
+ //FOURTH FUNCTION
+ function reverseWords(str) {
+   return str.split(" ").map(function(a) { 
+     return a.split("")}).map(function(c) {return c.reverse().join("")}).join(" ");
+ }
+
+ console.log(reverseWords(" A fun little challenge! "));
+
 
 
 
