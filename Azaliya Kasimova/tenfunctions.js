39e2b23e83e73f310e16e4714e6f43d812201f40
Azaliya Kasimova <sea-blue@inbox.ru>
2019-07-15 19:16:50 +0300

    function 6 added

--- Azaliya Kasimova/tenfunctions.js
+++ Azaliya Kasimova/tenfunctions.js
@@ -1,46 +1,66 @@
-
 //FUNCTION #1
 function splitAndMerge(str, sp) {
-  return str.split(" ").map(function(word) { return word.split("").join(sp)}).join(" ");
- }
-
- console.log(splitAndMerge("My name is John", " "));
- console.log(splitAndMerge("Hello World!", ","));
-
- //FUNCTION #2
-function convertToArray(obj) {
-   return Object.keys(obj).map(function(clue) { return [clue, obj[clue]]; })
+    return str.split(" ").map(function(word) { return word.split("").join(sp) }).join(" ");
 }
 
- console.log(convertToArray({name: 'Jeremy', age: 24, role: 'Software Engineer'}));
+console.log(splitAndMerge("My name is John", " "));
+console.log(splitAndMerge("Hello World!", ","));
 
- //FUNCTION #3
- function toCamelCase(str) {
-   var a = str.search("-");
-    if ( a > 0) {
-      sp = "-";
+//FUNCTION #2
+function convert(obj) {
+    return Object.keys(obj).map(function(clue) { return [clue, obj[clue]]; })
+}
+
+console.log(convert({ name: 'Jeremy', age: 24, role: 'Software Engineer' }));
+
+//FUNCTION #3
+function toCamelCase(str) {
+    var a = str.search("-");
+    if (a > 0) {
+        sp = "-";
+    } else {
+        sp = "_";
     }
-    else {
-       sp = "_";
-    }
-       
-    return str[0] + str.split(sp).map(function(word) { 
-      return word[0].toUpperCase() + word.substr(1);
+
+    return str[0] + str.split(sp).map(function(word) {
+        return word[0].toUpperCase() + word.substr(1);
     }).join("").substr(1);
- }
+}
 
- console.log(toCamelCase("the-stealth-warrior"));
- console.log(toCamelCase("The_Stealth_Warrior"));
+console.log(toCamelCase("the-stealth-warrior"));
+console.log(toCamelCase("The_Stealth_Warrior"));
 
- //FUNCTION #4
- function reverseWords(str) {
-   return str.split(" ").map(function(word) { 
-     return word.split("")}).map(function(char) {return char.reverse().join("")}).join(" ");
- }
+//FUNCTION #4
+function reverseWords(str) {
+    return str.split(" ").map(function(word) {
+        return word.split("")
+    }).map(function(char) { return char.reverse().join("") }).join(" ");
+}
 
- console.log(reverseWords(" A fun little challenge! "));
+console.log(reverseWords(" A fun little challenge! "));
 
+//FUNCTION #5
+/*function stringExpansion(str) {
+  return 
+}
 
+console.log('3D2a5d2f');
+console.log('3d332f2a');
+console.log('abcde');*/
 
+//FUNCTION #6
+function sortNumber(a, b) {
+    return a - b;
+}
 
- 
+function largest() {
+    return Array.from(arguments).sort(sortNumber).reverse()[0];
+}
+
+function smallest() {
+    return Array.from(arguments).sort(sortNumber)[0];
+
+}
+
+console.log(largest(2, 0.1, -5, 100, 3));
+console.log(smallest(2, 0.1, -5, 100, 3));
