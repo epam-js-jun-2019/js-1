fa9b5d7e6ba34bdd988567dfc9b58f37ed2bf9e1
TigranKirakosov <tkirakosov@mail.ru>
2019-07-16 18:52:12 +0400

    Done with second version of 1st function

--- TigranKirakosov/functions.js
+++ TigranKirakosov/functions.js
@@ -1,11 +1,12 @@
 // 1) splitAndMerge
 
+//version 1
 function splitAndMerge(str, sp) {
   let acc = [];
   let newStr = '';
   for (let i = 0; i < str.length; i++) {
-    if (str[i] == ' ') {
-      acc[i] = ' ';
+    if (str[i] === ' ') {
+      acc[i] = str[i];
     } else {
       if (str[i + 1] === ' ' || i === str.length - 1) {
         acc[i] = str[i];
@@ -15,5 +16,16 @@ function splitAndMerge(str, sp) {
     }
     newStr += acc[i];
   }
+  return newStr;
+}
+
+//version 2
+function splitAndMerge2(str, sp) {
+  let newStr = '';
+  str.split(' ').forEach((word, index, arr) => {
+    index == arr.length - 1
+      ? (newStr += word.split('').join(sp))
+      : (newStr += word.split('').join(sp) + ' ');
+  });
   return newStr;
 }
