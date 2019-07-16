6067e4fe0e9af915b643a8b86fc3601c0b6f1113
eroslyakov <ya.stinson@yandex.ru>
2019-07-16 23:21:19 +0300

    add the last five functions

--- Roslyakov Evgeny/hw_js_funcs.js
+++ Roslyakov Evgeny/hw_js_funcs.js
@@ -9,20 +9,20 @@ function splitAndMerge(str, sp) {
             return newWord.join(sp)
         })
         .reduce(function(acc, cur) {
-            return acc + ' ' + cur + ' ';
+            return acc + ' ' + cur + ' '
         })
         .trim();
 }
 
-console.log(splitAndMerge("My name is John"," "));
-console.log(splitAndMerge("Hello World!",","));
+// console.log(splitAndMerge("My name is John"," "));
+// console.log(splitAndMerge("Hello World!",","));
 
 
 // ---------- 2 ---------- //
 
 function convert(hash) {
     var res = [];
-    var temp = [];
+        temp = [];
     for(key in hash) {
         temp.push(key, hash[key]);
         res.push(temp);
@@ -32,11 +32,10 @@ function convert(hash) {
     return res;
 }
 
-console.log(convert({name: 'Jeremy', age: 24, role: 'Software Engineer'}));
+// console.log(convert({name: 'Jeremy', age: 24, role: 'Software Engineer'}));
 
 
 // ---------- 3 ---------- //
-
 
 function toCamelCase(str) {
     return str.split('')
@@ -49,39 +48,115 @@ function toCamelCase(str) {
         })
 }
 
-console.log(toCamelCase("the-stealth-warrior"));
-console.log(toCamelCase("The_Stealth_Warrior"));
+// console.log(toCamelCase("the-stealth-warrior"));
+// console.log(toCamelCase("The_Stealth_Warrior"));
 
 
 // ---------- 4 ---------- //
 
 function reverse(str) {
-    var temp = str.split('');
-    var res = '';
+    var temp = str.split(''),
+        res = '';
     for (var i = temp.length - 1; i >= 0; i--) {
         res += temp[i];
     }
     return res;
 }
 
-console.log(reverse(" A fun little challenge! "));
+// console.log(reverse(" A fun little challenge! "));
 
 
 // ---------- 5 ---------- //
 
 function stringExpansion(str) {
-    var temp = str.split('');
-    return temp.reduce(function(acc, cur, i, arr) {
-        if(Number.isInteger(+cur)) {
-            if(Number.isInteger(+arr[i+1])) {
-                return acc;
+    return str.split('')
+        .reduce(function(acc, cur, i, arr) {
+            if(Number.isInteger(+cur)) {
+                if(Number.isInteger(+arr[i+1])) {
+                    return acc;
+                }
+                return acc += new Array(+cur).join(arr[i+1]);
             }
-            return acc += new Array(+cur).join(arr[i+1]);
-        }
-        return acc += cur;
-    }, '')
+            return acc += cur;
+        }, '')
 }
 
-console.log(stringExpansion('3D2a5d2f'));
-console.log(stringExpansion('3d332f2a'));
-console.log(stringExpansion('abcde'));
+// console.log(stringExpansion('3D2a5d2f'));
+// console.log(stringExpansion('3d332f2a'));
+// console.log(stringExpansion('abcde'));
+
+
+// ---------- 6 ---------- //
+
+function largest() {
+    return Math.max.apply(null, arguments);
+}
+
+function smallest() {
+    return Math.min.apply(null, arguments);
+}
+
+// console.log(largest(2, 0.1, -5, 100, 3));
+// console.log(smallest(2, 0.1, -5, 100, 3));
+
+
+// ---------- 7 ---------- //
+
+function transform(baseArray) {
+    return baseArray.map(function(elem, i, arr) {
+        elem = function() {
+            var idx = i;
+            var refArr = arr;
+            console.log(refArr[idx]);
+        };
+        return elem;
+    })
+}
+
+// var newArray = transform([10, 20, 30, 40, 50]);
+// newArray[3]();
+// newArray[4]();
+
+
+// ---------- 8 ---------- //
+
+function sum() {
+    if(arguments.length !== 1) {
+        return arguments[0] + sum.apply(null, [].slice.call(arguments, 1));
+    } else {
+        return arguments[0];
+    }
+}
+
+// console.log(sum(1,3,5,7));
+
+
+// ---------- 9 ---------- //
+
+function countDown(num) {
+    var newTimer = setInterval(function() {
+        if(num === 0) clearInterval(newTimer);
+        console.log(num);
+        num -= 1;
+    }, 1000);
+}
+
+// countDown(3);
+
+
+// ---------- 10 ---------- //
+
+Function.prototype.myBind = function() {
+    var self = this,
+        context = arguments[0],
+        currArgs = [].slice.call(arguments, 1);
+    return function() {
+        var args = [].slice.call(arguments),
+            newArgs = currArgs.concat(args);
+        return self.apply(context, newArgs);
+    }
+}
+
+function addPropToNumber(number) { return this.prop + number; };
+var bound = addPropToNumber.myBind({ prop: 9 });
+// console.log(bound(1))
