d5f7c48e59eb7459b041fbdba170e750adbff29e
TigranKirakosov <tkirakosov@mail.ru>
2019-07-17 14:40:08 +0400

    Done with 5th func & fixed other

--- TigranKirakosov/functions.js
+++ TigranKirakosov/functions.js
@@ -1,10 +1,11 @@
+'use strict'
 // 1) splitAndMerge
 
 //version 1
 function splitAndMerge(str, sp) {
-  let acc = [];
-  let newStr = '';
-  for (let i = 0; i < str.length; i++) {
+  var acc = [];
+  var newStr = '';
+  for (var i = 0; i < str.length; i++) {
     if (str[i] === ' ') {
       acc[i] = str[i];
     } else {
@@ -21,7 +22,7 @@ function splitAndMerge(str, sp) {
 
 //version 2
 function splitAndMerge2(str, sp) {
-  let newStr = '';
+  var newStr = '';
   str.split(' ').forEach((word, index, arr) => {
     index === arr.length - 1
       ? (newStr += word.split('').join(sp))
@@ -30,37 +31,90 @@ function splitAndMerge2(str, sp) {
   return newStr;
 }
 
+//version 3
+function splitAndMerge3(str, sp) {
+  return str
+    .split(' ')
+    .map(word => word.split('').join(sp))
+    .join(' ');
+}
+
 // 2) hashToArr
 function hashToArr(hash) {
   return Object.keys(hash).map(prop => [prop, hash[prop]]);
 }
 
 // 3) toCamelCase
+
+// invalid (ES6) version 1
 function toCamelCase(str) {
-  let newStr = '';
-  let acc;
+  var newStr = '';
+  var acc;
   if (str.includes('-')) acc = str.split('-');
   else if (str.includes('_')) acc = str.split('_');
   else if (str.includes(' ')) acc = str.split(' ');
   else return str;
-  acc
-    .map((word, index) => {
-      if (index === 0) newStr += word;
-      else if (word[0] !== undefined) {
-        newStr += word[0].toUpperCase() + word.slice(1);
-      }
-    })
-    .join('');
+  acc.forEach((word, index) => {
+    if (index === 0) newStr += word;
+    else if (word[0] !== undefined) {
+      newStr += word[0].toUpperCase() + word.slice(1);
+    }
+  });
   return newStr;
 }
 
+//version 2
+function toCamelCase2(str) {
+  var sp;
+  var re = /\-|\_| /;
+  if (str.match(re)) {
+    sp = re;
+  }
+  return str.split(sp).map((word, index) => {
+    if (index !== 0) {
+      return word[0].toUpperCase() + word.slice(1)
+    } else {
+      return word
+    }
+  }).join('')
+}
+
 // 4) wordReverser
+
+// version 1
 function wordReverser(str) {
-  let newStr = '';
+  var newStr = '';
   str.split(' ').map((word, index, arr) => {
     index === arr.length - 1
       ? (newStr += word.split('').reverse().join(''))
       : (newStr += word.split('').reverse().join('') + ' ');
   });
   return newStr;
+}
+
+// version 2
+function wordReverser2(str) {
+  return str
+    .split(' ')
+    .map(word => word.split('').reverse().join(''))
+    .join(' ');
+}
+
+// 5) stringExpansion
+function stringExpansion(str) {
+  function strRepeat(str, num) {
+    var repStr = '';
+    while (num - 1 > 0) {
+      repStr += str;
+      num--;
+    }
+    return repStr;
+  }
+  var newStr = [];
+  for (var i = 0; i < str.length; i++) {
+    (+str[i] && !(+str[i + 1]))
+      ? newStr[i] = strRepeat(str[i + 1], +str[i])
+      : newStr[i] = str[i];
+  }
+  return newStr.filter(char => !(+char)).join('');
 }
